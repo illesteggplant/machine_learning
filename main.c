f6dd9d9f476d791b7c5f671ae60387cadabe50ee
illesteggplant <samwilliamson317@gmail.com>
2023-10-29 12:50:30 -0400

    ml initial commit

--- main.c
+++ main.c
@@ -1,7 +1,49 @@
 #include <stdio.h>  
+#include <stdlib.h>  
+#include <time.h>
+
+float train[][2] = 
+{
+    {0, 0},
+    {1, 2},
+    {2, 4},
+    {3, 6},
+    {4, 8},
+};
+
+#define train_count (sizeof(train)/sizeof(train[0]))
+
+float rand_float(void)
+{
+    // get random number from 0 -> 1
+    return (float) rand() / (float) RAND_MAX;
+}
+
+// cost function
+float cost(float w)
+{
+    float result = 0.0f;
+    for( size_t i = 0; i < train_count; ++i )
+    {
+        float x = train[i][0];
+        float y = x*w;
+        float d = y - train[i][1];
+        result += d*d;
+    }
+    result /= train_count;
+    return result;
+}
 
 int main()
 {
-    printf("Hello, World!");
+    srand(69);
+    // y = x*w
+    float w = rand_float()*10.0f;
+
+    float eps = 1e-3;
+    printf("%f\n", cost(w));
+    printf("%f\n", cost(w + eps));
+    printf("%f\n", cost(w + eps*2));
+    printf("%f\n", cost(w + eps*3));
     return 0;
 }
